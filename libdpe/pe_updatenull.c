e20d26964df4ab9ce5da7a1a11f5e62dbf8e3762
Peter Jones <pjones@redhat.com>
2012-01-20 15:37:46 -0500

    Simple implementations of pe_default_{mz,pe}hdr().
    We'll need these someday.

--- libdpe/pe_updatenull.c
+++ libdpe/pe_updatenull.c
@@ -19,6 +19,29 @@
 
 #include "libdpe.h"
 
+static int
+pe_default_mzhdr(Pe *pe, struct mz_hdr *mzhdr)
+{
+	/* Always write the mz magic */
+	uint16_t mz_magic = cpu_to_le16(MZ_MAGIC);
+	update_if_changed(mzhdr->magic, mz_magic, pe->flags);
+
+	/* XXX FIXME: write a real MZ header */
+	/* print "the only way to win is not to play" */
+	return 0;
+}
+
+static int
+pe_default_pehdr(Pe *pe, struct pe_hdr *pehdr, size_t shnum)
+{
+	/* Always write the pe magic */
+	uint32_t pe_magic = cpu_to_le32(PE_MAGIC);
+	update_if_changed(pehdr->magic, pe_magic, pe->flags);
+
+	return 0;
+}
+
+
 off_t
 __pe_updatenull_wrlock(Pe *pe, size_t shnum)
 {
