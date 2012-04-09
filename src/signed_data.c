4a9decf1cab035c73da0c44563bf440346e38e3a
Peter Jones <pjones@redhat.com>
2012-04-09 11:15:25 -0400

    Get rid of #if 0'd code we're never going to wind up using.

--- src/signed_data.c
+++ src/signed_data.c
@@ -252,7 +252,7 @@ generate_spc_signed_data(SECItem *sdp, cms_context *ctx)
 	memcpy(&sdw.content, &encoded, sizeof (sdw.content));
 
 	SECItem wrapper = { 0, };
-	if (SEC_ASN1EncodeItem(ctx->arena, &wrapper, &sdw,
+	if (SEC_ASN1Encode(ctx->arena, &wrapper, &sdw,
 			ContentInfoTemplate) == NULL) {
 		fprintf(stderr, "Could not encode SignedData: %s\n",
 			PORT_ErrorToString(PORT_GetError()));
@@ -277,8 +277,15 @@ err:
 
 	
 #if 0
-	NSSCMSMessage *cmsg = NULL;
-
+	typedef struct {
+		enum {
+			PW_NONE = 0,
+			PW_FROMFILE = 1,
+			PW_PLAINTEXT = 2,
+			PW_EXTERNAL = 3
+		} source;
+		char *data;
+	} secuPWData;
 	secuPWData pwdata = { 0, 0 };
 	CERTCertificate *cert = NULL;
 
@@ -288,126 +295,5 @@ err:
 		fprintf(stderr, "Could not find certificate\n");
 		goto err;
 	}
-
-	cmsg = NSS_CMSMessage_Create(NULL);
-	if (cmsg == NULL) {
-		fprintf(stderr, "Could not create CMS Message\n");
-		goto err;
-	}
-
-	NSSCMSSignedData *sigd = NULL;
-	sigd = NSS_CMSSignedData_Create(cmsg);
-	if (sigd == NULL) {
-		fprintf(stderr, "Could not create Signed Data\n");
-		goto err;
-	}
-
-	NSSCMSContentInfo *cinfo;
-	cinfo = NSS_CMSMessage_GetContentInfo(cmsg);
-	if (NSS_CMSContentInfo_SetContent_SignedData(cmsg, cinfo, sigd) !=
-			SECSuccess) {
-		fprintf(stderr, "Could not set Signed Data\n");
-		goto err;
-	}
-	//NSS_CMSContentInfo_SetDontStream(cinfo, PR_TRUE);
-
-	cinfo = NSS_CMSSignedData_GetContentInfo(sigd);
-	SECOidTag tag = find_ms_oid_tag(SPC_INDIRECT_DATA_OBJID);
-	if (NSS_CMSContentInfo_SetContent(cmsg, cinfo, tag, cip) !=
-			SECSuccess) {
-		fprintf(stderr, "Could not set Data\n");
-		goto err;
-	}
-	//NSS_CMSContentInfo_SetDontStream(cinfo, PR_TRUE);
-
-	NSSCMSSignerInfo *signerinfo = NULL;
-	signerinfo = NSS_CMSSignerInfo_Create(cmsg, cert, hashalg);
-	if (signerinfo == NULL) {
-		fprintf(stderr, "Could not create Signer Info\n");
-		goto err;
-	}
-
-	if (NSS_CMSSignerInfo_IncludeCerts(signerinfo, NSSCMSCM_CertChain,
-			certUsageObjectSigner) != SECSuccess) {
-		fprintf(stderr, "Could not include certs in signer info\n");
-		goto err;
-	}
-
-	if (NSS_CMSSignerInfo_AddSigningTime(signerinfo, PR_Now()) !=
-			SECSuccess) {
-		fprintf(stderr, "Could not add signing time\n");
-		goto err;
-	}
-
-	if (NSS_CMSSignedData_AddSignerInfo(sigd, signerinfo) != SECSuccess) {
-		fprintf(stderr, "Could not add Signer Info\n");
-		goto err;
-	}
-
-	PRArenaPool *arena = PORT_NewArena(DER_DEFAULT_CHUNKSIZE);
-	if (!arena) {
-		fprintf(stderr, "Could not create new arena\n");
-		goto err;
-	}
-
-	NSSCMSEncoderContext *ecx = NULL;
-	ecx = NSS_CMSEncoder_Start(cmsg,
-				NULL, NULL,
-				sdp, arena,
-				NULL, NULL,
-				NULL, NULL,
-				NULL, NULL);
-	if (!ecx) {
-		fprintf(stderr, "Could not create encoder context: %s\n",
-			PORT_ErrorToString(PORT_GetError()));
-		goto err;
-	}
-
-	SECStatus rv;
-	rv = NSS_CMSEncoder_Update(ecx, (char *)cip, cip->content.len + cip->contentType.len);
-	if (rv != SECSuccess) {
-		fprintf(stderr, "Failed to add content: %s\n",
-			PORT_ErrorToString(PORT_GetError()));
-		goto err;
-	}
-
-	rv = NSS_CMSEncoder_Finish(ecx);
-	if (rv != SECSuccess) {
-		fprintf(stderr, "Failed to encode data: %s\n",
-			PORT_ErrorToString(PORT_GetError()));
-		ecx = NULL;
-		sdp->data = NULL;
-		sdp->len = 0;
-		goto err;
-	}
-	ecx = NULL;
-
-	rc = 0;
-err:
-	if (ecx)
-		NSS_CMSEncoder_Cancel(ecx);
-	
-	if (arena) /* XXX use PR_TRUE, which means duplicating sdp before
-			returning */
-		PORT_FreeArena(arena, PR_FALSE);
-
-	if (cert)
-		CERT_DestroyCertificate(cert);
-
-	if (cmsg)
-		NSS_CMSMessage_Destroy(cmsg);
-	return rc;
 #endif
 }
-
-#if 0
-typedef struct {
-	enum {
-		PW_NONE = 0,
-		PW_FROMFILE = 1,
-		PW_PLAINTEXT = 2,
-		PW_EXTERNAL = 3
-	} source;
-	char *data;
-} secuPWData;
-#endif
