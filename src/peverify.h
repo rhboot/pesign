daecc507698b68c6b473b39941c5520c1b65083d
Peter Jones <pjones@redhat.com>
2012-10-01 10:08:38 -0400

    Do include peverify.h in case somebody /wants/ to build it.

---
+++ src/peverify.h
@@ -0,0 +1,39 @@
+/*
+ * Copyright 2011 Red Hat, Inc.
+ * All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Author(s): Peter Jones <pjones@redhat.com>
+ */
+#ifndef PESIGN_H
+#define PESIGN_H 1
+
+#include <libdpe/libdpe.h>
+#include <libdpe/pe.h>
+
+#include "efitypes.h"
+#include "cms_common.h"
+#include "peverify_context.h"
+#include "certdb.h"
+
+#include "util.h"
+#include "endian.h"
+#include "oid.h"
+#include "wincert.h"
+#include "content_info.h"
+#include "signer_info.h"
+#include "signed_data.h"
+#include "password.h"
+
+#endif /* PESIGN_H */
