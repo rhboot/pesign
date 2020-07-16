f97d3b04a2eafb42272ede24e1353dd0a7f4347c
Peter Jones <pjones@redhat.com>
2020-07-16 16:28:26 -0400

    Use /run not /var/run
    Signed-off-by: Peter Jones <pjones@redhat.com>
    [rharwood@redhat.com: pesign-authorize order changes]
    Signed-off-by: Robbie Harwood <rharwood@redhat.com>

--- src/daemon.h
+++ src/daemon.h
@@ -51,8 +51,8 @@ typedef enum {
 } pesignd_cmd;
 
 #define PESIGND_VERSION 0x2a9edaf0
-#define SOCKPATH	"/var/run/pesign/socket"
-#define PIDFILE		"/var/run/pesign.pid"
+#define SOCKPATH	"/run/pesign/socket"
+#define PIDFILE		"/run/pesign.pid"
 
 static inline uint32_t UNUSED
 pesignd_string_size(char *buffer)
