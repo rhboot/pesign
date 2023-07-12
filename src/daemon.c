rhboot/pesign

commit 2b3ca2b953140a70cbf6ee1e652321671b5508b4

fix a couple of minor nits scan-build found.
Signed-off-by: Peter Jones <pjones@redhat.com>

src/daemon.c
@@ -1190,8 +1190,10 @@ daemonize(cms_context *cms_ctx, char *certdir, int do_fork)
 			sleep(2);
 			return 0;
 		}
+		ctx.pid = pid;
+	} else {
+		ctx.pid = getpid();
 	}
-	ctx.pid = getpid();
 	write_pid_file(ctx.pid);
 	ctx.backup_cms->log(ctx.backup_cms, ctx.priority|LOG_NOTICE,
 		"pesignd starting (pid %d)", ctx.pid);

src/client.c
@@ -140,6 +140,9 @@ check_cmd_version(int sd, uint32_t command, char *name, int32_t version, bool do
 	char *srvmsg = NULL;
 	int32_t rc = check_response(sd, &srvmsg);
 
+	if (srvmsg)
+		free(srvmsg);
+
 	if (do_exit && rc < 0)
 		errx(1, "command \"%s\" not known by server", name);
 	if (do_exit && rc != version)

src/cms_common.c
@@ -1560,8 +1560,7 @@ generate_name(cms_context *cms, SECItem *der, CERTName *certname)
 
 	while (rdns && (rdn = *rdns++) != NULL) {
 		CERTAVA **avas = rdn->avas;
-		CERTAVA *ava;
-		while (avas && (ava = *avas++) != NULL)
+		while (avas && ((*avas++) != NULL))
 			num_items++;
 	}
 

commit 3eb106f8535af2c055a5e85c4679ff952398806c

daemon: fix pid value by using getpid() in the child process
Previously ctx.pid was getting it's value indirectly from fork(),
which is always zero in the child process.

Fixed a bug introduced in 2b3ca2b.

Signed-off-by: Egor Ignatov <egori@altlinux.org>

src/daemon.c
@@ -1191,10 +1191,8 @@ daemonize(cms_context *cms_ctx, char *certdir, int do_fork)
 			sleep(2);
 			return 0;
 		}
-		ctx.pid = pid;
-	} else {
-		ctx.pid = getpid();
 	}
+	ctx.pid = getpid();
 	write_pid_file(ctx.pid);
 	ctx.backup_cms->log(ctx.backup_cms, ctx.priority|LOG_NOTICE,
 		"pesignd starting (pid %d)", ctx.pid);