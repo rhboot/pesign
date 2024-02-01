rhboot/pesign

commit 12f16710ee44ef64ddb044a3523c3c4c4d90039a

Rework the wildly undocumented NSS password file goo.
This probably doesn't work yet.

src/cms_common.c
@@ -152,10 +152,30 @@ cms_context_fini(cms_context *cms)
 		cms->cert = NULL;
 	}
 
+	switch (cms->pwdata.source) {
+	case PW_SOURCE_INVALID:
+	case PW_PROMPT:
+	case PW_DEVICE:
+	case PW_FROMFILEDB:
+	case PW_FROMENV:
+	case PW_SOURCE_MAX:
+		break;
+	case PW_DATABASE:
+		xfree(cms->pwdata.data);
+		break;
+	case PW_PLAINTEXT:
+		memset(cms->pwdata.data, 0, strlen(cms->pwdata.data));
+		xfree(cms->pwdata.data);
+		break;
+	}
+	cms->pwdata.source = PW_SOURCE_INVALID;
+	cms->pwdata.orig_source = PW_SOURCE_INVALID;
+
 	if (cms->privkey) {
 		free(cms->privkey);
 		cms->privkey = NULL;
 	}
+
 
 	/* These were freed when the arena was destroyed */
 	if (cms->tokenname)
@@ -243,9 +263,35 @@ void cms_set_pw_callback(cms_context *cms, PK11PasswordFunc func)
 	cms->func = func;
 }
 
-void cms_set_pw_data(cms_context *cms, void *pwdata)
+void cms_set_pw_data(cms_context *cms, secuPWData *pwdata)
 {
-	cms->pwdata = pwdata;
+	ingress();
+
+	switch (cms->pwdata.source) {
+	case PW_SOURCE_INVALID:
+	case PW_PROMPT:
+	case PW_DEVICE:
+	case PW_SOURCE_MAX:
+		break;
+
+	case PW_FROMENV:
+	case PW_FROMFILEDB:
+	case PW_PLAINTEXT:
+		memset(cms->pwdata.data, 0, strlen(cms->pwdata.data));
+		xfree(cms->pwdata.data);
+		break;
+
+	case PW_DATABASE:
+		xfree(cms->pwdata.data);
+		break;
+	}
+	memmove(&cms->pwdata, pwdata, sizeof(*pwdata));
+
+	dprintf("pwdata:%p", pwdata);
+	dprintf("pwdata->source:%d", pwdata->source);
+	dprintf("pwdata->data:%p (\"%s\")", pwdata->data,
+		pwdata->data ? pwdata->data : "(null)");
+	egress();
 }
 
 int
@@ -268,24 +314,26 @@ set_digest_parameters(cms_context *cms, char *name)
 	return -1;
 }
 
-struct cbdata {
-	CERTCertificate *cert;
+struct validity_cbdata {
+	cms_context *cms;
 	PK11SlotListElement *psle;
-	secuPWData *pwdata;
+	PK11SlotInfo *slot;
+	CERTCertificate *cert;
 };
 
 static SECStatus
 is_valid_cert(CERTCertificate *cert, void *data)
 {
-	struct cbdata *cbdata = (struct cbdata *)data;
-
-	PK11SlotInfo *slot = cbdata->psle->slot;
-	void *pwdata = cbdata->pwdata;
-
+	struct validity_cbdata *cbd = (struct validity_cbdata *)data;
+	PK11SlotInfo *slot = cbd->slot;
 	SECKEYPrivateKey *privkey = NULL;
-	privkey = PK11_FindPrivateKeyFromCert(slot, cert, pwdata);
+
+	if (cert == NULL)
+		return SECFailure;
+
+	privkey = PK11_FindPrivateKeyFromCert(slot, cert, cbd->cms);
 	if (privkey != NULL) {
-		cbdata->cert = cert;
+		cbd->cert = CERT_DupCertificate(cert);
 		SECKEY_DestroyPrivateKey(privkey);
 		return SECSuccess;
 	}
@@ -295,9 +343,15 @@ is_valid_cert(CERTCertificate *cert, void *data)
 static SECStatus
 is_valid_cert_without_private_key(CERTCertificate *cert, void *data)
 {
-	struct cbdata *cbdata = (struct cbdata *)data;
-	if (cert) {
-		cbdata->cert = cert;
+	struct validity_cbdata *cbd = (struct validity_cbdata *)data;
+	PK11SlotInfo *slot = cbd->slot;
+	SECKEYPrivateKey *privkey = NULL;
+
+	if (cert == NULL)
+		return SECFailure;
+	privkey = PK11_FindPrivateKeyFromCert(slot, cert, cbd->cms);
+	if (privkey == NULL) {
+		cbd->cert = CERT_DupCertificate(cert);
 		return SECSuccess;
 	}
 	return SECFailure;
@@ -326,12 +380,10 @@ PK11_DestroySlotListElement(PK11SlotList *slots, PK11SlotListElement **psle)
 int
 unlock_nss_token(cms_context *cms)
 {
-	secuPWData pwdata_val = { 0, 0 };
-	void *pwdata = cms->pwdata ? cms->pwdata : &pwdata_val;
 	PK11_SetPasswordFunc(cms->func ? cms->func : SECU_GetModulePassword);
 
 	PK11SlotList *slots = NULL;
-	slots = PK11_GetAllTokens(CKM_RSA_PKCS, PR_FALSE, PR_TRUE, pwdata);
+	slots = PK11_GetAllTokens(CKM_RSA_PKCS, PR_FALSE, PR_TRUE, cms);
 	if (!slots)
 		cmsreterr(-1, cms, "could not get pk11 token list");
 
@@ -355,20 +407,21 @@ unlock_nss_token(cms_context *cms)
 		save_port_err() {
 			PK11_FreeSlotList(slots);
 		}
-		cms->log(cms, LOG_ERR, "could not find token \"%s\"",
-			cms->tokenname);
-		return -1;
+		nssreterr(-1, "Could not find token \"%s\"", cms->tokenname);
 	}
 
 	SECStatus status;
 	if (PK11_NeedLogin(psle->slot) &&
-			!PK11_IsLoggedIn(psle->slot, pwdata)) {
-		status = PK11_Authenticate(psle->slot, PR_TRUE, pwdata);
+			!PK11_IsLoggedIn(psle->slot, cms)) {
+		status = PK11_Authenticate(psle->slot, PR_TRUE, cms);
 		if (status != SECSuccess) {
-			PK11_DestroySlotListElement(slots, &psle);
-			PK11_FreeSlotList(slots);
-			cms->log(cms, LOG_ERR, "authentication failed for "
-				"token \"%s\"", cms->tokenname);
+			save_port_err() {
+				PK11_DestroySlotListElement(slots, &psle);
+				PK11_FreeSlotList(slots);
+				cms->log(cms, LOG_ERR,
+					 "authentication failed for token \"%s\"",
+					 cms->tokenname);
+			}
 			return -1;
 		}
 	}
@@ -381,17 +434,16 @@ unlock_nss_token(cms_context *cms)
 int
 find_certificate(cms_context *cms, int needs_private_key)
 {
+	struct validity_cbdata cbd;
 	if (!cms->certname || !*cms->certname) {
 		cms->log(cms, LOG_ERR, "no certificate name specified");
 		return -1;
 	}
 
-	secuPWData pwdata_val = { 0, 0 };
-	void *pwdata = cms->pwdata ? cms->pwdata : &pwdata_val;
 	PK11_SetPasswordFunc(cms->func ? cms->func : SECU_GetModulePassword);
 
 	PK11SlotList *slots = NULL;
-	slots = PK11_GetAllTokens(CKM_RSA_PKCS, PR_FALSE, PR_TRUE, pwdata);
+	slots = PK11_GetAllTokens(CKM_RSA_PKCS, PR_FALSE, PR_TRUE, cms);
 	if (!slots)
 		cmsreterr(-1, cms, "could not get pk11 token list");
 
@@ -415,19 +467,20 @@ find_certificate(cms_context *cms, int needs_private_key)
 		save_port_err() {
 			PK11_FreeSlotList(slots);
 		}
-		cms->log(cms, LOG_ERR, "could not find token \"%s\"",
-			cms->tokenname);
-		return -1;
+		nssreterr(-1, "Could not find token \"%s\"", cms->tokenname);
 	}
 
 	SECStatus status;
-	if (PK11_NeedLogin(psle->slot) && !PK11_IsLoggedIn(psle->slot, pwdata)) {
-		status = PK11_Authenticate(psle->slot, PR_TRUE, pwdata);
+	if (PK11_NeedLogin(psle->slot) && !PK11_IsLoggedIn(psle->slot, cms)) {
+		status = PK11_Authenticate(psle->slot, PR_TRUE, cms);
 		if (status != SECSuccess) {
-			PK11_DestroySlotListElement(slots, &psle);
-			PK11_FreeSlotList(slots);
-			cms->log(cms, LOG_ERR, "authentication failed for "
-				"token \"%s\"", cms->tokenname);
+			save_port_err() {
+				PK11_DestroySlotListElement(slots, &psle);
+				PK11_FreeSlotList(slots);
+				cms->log(cms, LOG_ERR,
+					 "authentication failed for token \"%s\"",
+					 cms->tokenname);
+			}
 			return -1;
 		}
 	}
@@ -447,35 +500,37 @@ find_certificate(cms_context *cms, int needs_private_key)
 		.len = strlen(cms->certname) + 1,
 		.type = siUTF8String,
 	};
-	struct cbdata cbdata = {
-		.cert = NULL,
-		.psle = psle,
-		.pwdata = pwdata,
-	};
+
+	cms->psle = psle;
+
+	cbd.cms = cms;
+	cbd.psle = psle;
+	cbd.slot = psle->slot;
+	cbd.cert = NULL;
 
 	if (needs_private_key) {
 		status = PK11_TraverseCertsForNicknameInSlot(&nickname,
-					psle->slot, is_valid_cert, &cbdata);
+					psle->slot, is_valid_cert, &cbd);
 	} else {
 		status = PK11_TraverseCertsForNicknameInSlot(&nickname,
 					psle->slot,
 					is_valid_cert_without_private_key,
-					&cbdata);
+					&cbd);
 	}
-	if (status != SECSuccess || cbdata.cert == NULL) {
-		save_port_err() {
-			CERT_DestroyCertList(certlist);
-			PK11_DestroySlotListElement(slots, &psle);
-			PK11_FreeSlotList(slots);
-		}
+	if (status == SECSuccess && cbd.cert != NULL) {
+		if (cms->cert)
+			CERT_DestroyCertificate(cms->cert);
+		cms->cert = CERT_DupCertificate(cbd.cert);
+	}
+
+	save_port_err() {
+		CERT_DestroyCertList(certlist);
+		PK11_DestroySlotListElement(slots, &psle);
+		PK11_FreeSlotList(slots);
+		cms->psle = NULL;
+	}
+	if (status != SECSuccess || cms->cert == NULL)
 		cmsreterr(-1, cms, "could not find certificate in list");
-	}
-
-	cms->cert = CERT_DupCertificate(cbdata.cert);
-
-	PK11_DestroySlotListElement(slots, &psle);
-	PK11_FreeSlotList(slots);
-	CERT_DestroyCertList(certlist);
 
 	return 0;
 }
@@ -488,12 +543,10 @@ find_slot_for_token(cms_context *cms, PK11SlotInfo **slot)
 		return -1;
 	}
 
-	secuPWData pwdata_val = { 0, 0 };
-	void *pwdata = cms->pwdata ? cms->pwdata : &pwdata_val;
 	PK11_SetPasswordFunc(cms->func ? cms->func : SECU_GetModulePassword);
 
 	PK11SlotList *slots = NULL;
-	slots = PK11_GetAllTokens(CKM_RSA_PKCS, PR_FALSE, PR_TRUE, pwdata);
+	slots = PK11_GetAllTokens(CKM_RSA_PKCS, PR_FALSE, PR_TRUE, cms);
 	if (!slots)
 		cmsreterr(-1, cms, "could not get pk11 token list");
 
@@ -517,19 +570,20 @@ find_slot_for_token(cms_context *cms, PK11SlotInfo **slot)
 		save_port_err() {
 			PK11_FreeSlotList(slots);
 		}
-		cms->log(cms, LOG_ERR, "could not find token \"%s\"",
-			cms->tokenname);
-		return -1;
+		nssreterr(-1, "Could not find token \"%s\"", cms->tokenname);
 	}
 
 	SECStatus status;
-	if (PK11_NeedLogin(psle->slot) && !PK11_IsLoggedIn(psle->slot, pwdata)) {
-		status = PK11_Authenticate(psle->slot, PR_TRUE, pwdata);
+	if (PK11_NeedLogin(psle->slot) && !PK11_IsLoggedIn(psle->slot, cms)) {
+		status = PK11_Authenticate(psle->slot, PR_TRUE, cms);
 		if (status != SECSuccess) {
-			PK11_DestroySlotListElement(slots, &psle);
-			PK11_FreeSlotList(slots);
-			cms->log(cms, LOG_ERR, "authentication failed for "
-				"token \"%s\"", cms->tokenname);
+			save_port_err() {
+				PK11_DestroySlotListElement(slots, &psle);
+				PK11_FreeSlotList(slots);
+				cms->log(cms, LOG_ERR,
+					 "authentication failed for token \"%s\"",
+					 cms->tokenname);
+			}
 			return -1;
 		}
 	}
@@ -545,12 +599,10 @@ find_named_certificate(cms_context *cms, char *name, CERTCertificate **cert)
 		return -1;
 	}
 
-	secuPWData pwdata_val = { 0, 0 };
-	void *pwdata = cms->pwdata ? cms->pwdata : &pwdata_val;
 	PK11_SetPasswordFunc(cms->func ? cms->func : SECU_GetModulePassword);
 
 	PK11SlotList *slots = NULL;
-	slots = PK11_GetAllTokens(CKM_RSA_PKCS, PR_FALSE, PR_TRUE, pwdata);
+	slots = PK11_GetAllTokens(CKM_RSA_PKCS, PR_FALSE, PR_TRUE, cms);
 	if (!slots)
 		cmsreterr(-1, cms, "could not get pk11 token list");
 
@@ -573,20 +625,23 @@ find_named_certificate(cms_context *cms, char *name, CERTCertificate **cert)
 	if (!psle) {
 		save_port_err() {
 			PK11_FreeSlotList(slots);
+			cms->log(cms, LOG_ERR, "could not find token \"%s\"",
+				 cms->tokenname);
 		}
-		cms->log(cms, LOG_ERR, "could not find token \"%s\"",
-			cms->tokenname);
 		return -1;
 	}
 
 	SECStatus status;
-	if (PK11_NeedLogin(psle->slot) && !PK11_IsLoggedIn(psle->slot, pwdata)) {
-		status = PK11_Authenticate(psle->slot, PR_TRUE, pwdata);
+	if (PK11_NeedLogin(psle->slot) && !PK11_IsLoggedIn(psle->slot, cms)) {
+		status = PK11_Authenticate(psle->slot, PR_TRUE, cms);
 		if (status != SECSuccess) {
-			PK11_DestroySlotListElement(slots, &psle);
-			PK11_FreeSlotList(slots);
-			cms->log(cms, LOG_ERR, "authentication failed for "
-				"token \"%s\"", cms->tokenname);
+			save_port_err() {
+				PK11_DestroySlotListElement(slots, &psle);
+				PK11_FreeSlotList(slots);
+				cms->log(cms, LOG_ERR,
+					 "authentication failed for token \"%s\"",
+					 cms->tokenname);
+			}
 			return -1;
 		}
 	}
@@ -1480,14 +1535,14 @@ generate_keys(cms_context *cms, PK11SlotInfo *slot,
 	};
 
 	SECStatus rv;
-	rv = PK11_Authenticate(slot, PR_TRUE, cms->pwdata);
+	rv = PK11_Authenticate(slot, PR_TRUE, cms);
 	if (rv != SECSuccess)
 		cmsreterr(-1, cms, "could not authenticate with pk11 service");
 
 	void *params = &rsaparams;
 	*privkey = PK11_GenerateKeyPair(slot, CKM_RSA_PKCS_KEY_PAIR_GEN,
 					params, pubkey, PR_TRUE, PR_TRUE,
-					cms->pwdata);
+					cms);
 	if (!*privkey)
 		cmsreterr(-1, cms, "could not generate RSA keypair");
 	return 0;

src/pesign.c
@@ -60,6 +60,10 @@ long verbosity(void)
 	return *verbose;
 }
 
+enum {
+	POPT_RET_PWDB = 0x40000001
+};
+
 int
 main(int argc, char *argv[])
 {
@@ -82,6 +86,10 @@ main(int argc, char *argv[])
 	char *certname = NULL;
 	char *certdir = "/etc/pki/pesign";
 	char *signum = NULL;
+
+	secuPWData pwdata;
+
+	memset(&pwdata, 0, sizeof(pwdata));
 
 	setenv("NSS_DEFAULT_DB_TYPE", "sql", 0);
 
@@ -260,6 +268,12 @@ main(int argc, char *argv[])
 		 .arg = &check_vendor_cert,
 		 .val = 0,
 		 .descrip = "do not hash the .vendor_cert section." },
+		{.longName = "pwfile",
+		 .shortName = '\0',
+		 .argInfo = POPT_ARG_STRING|POPT_ARGFLAG_DOC_HIDDEN,
+		 .arg = &pwdata.data,
+		 .descrip = "file to read passwords from.",
+		 .argDescrip = "<pwfile>" },
 		POPT_AUTOALIAS
 		POPT_AUTOHELP
 		POPT_TABLEEND
@@ -274,8 +288,32 @@ main(int argc, char *argv[])
 		exit(1);
 	}
 
-	while ((rc = poptGetNextOpt(optCon)) > 0)
-		;
+	while ((rc = poptGetNextOpt(optCon)) > 0) {
+		switch (rc) {
+		case POPT_RET_PWDB:
+			dprintf("POPT_RET_PWDB:\"%s\"", pwdata.data ? pwdata.data : "(null)");
+			if (pwdata.source != PW_SOURCE_INVALID)
+				errx(1, "only one password/pin method can be used at a time");
+			if (pwdata.data == NULL)
+				errx(1, "--pwfile requires a file name as an argument");
+			pwdata.source = PW_FROMFILEDB;
+			pwdata.data = strdup(pwdata.data);
+			if (!pwdata.data)
+				err(1, "could not allocate memory");
+			continue;
+		}
+	}
+
+	dprintf("pwdata.source:%d %schecking for PESIGN_TOKEN_PIN",
+		pwdata.source,
+		pwdata.source == PW_SOURCE_INVALID ? "" : "not ");
+	if (pwdata.source == PW_SOURCE_INVALID && secure_getenv("PESIGN_TOKEN_PIN")) {
+		pwdata.source = PW_FROMENV;
+		pwdata.data = strdup(secure_getenv("PESIGN_TOKEN_PIN"));
+		if (!pwdata.data)
+			err(1, "could not allocate memory");
+	}
+	pwdata.orig_source = pwdata.source;
 
 	if (rc < -1) {
 		fprintf(stderr, "pesign: Invalid argument: %s: %s\n",
@@ -397,6 +435,9 @@ main(int argc, char *argv[])
 		}
 		exit(!is_help);
 	}
+
+	if (pwdata.source != PW_DEVICE)
+		cms_set_pw_data(ctxp->cms_ctx, &pwdata);
 
 	ctxp->cms_ctx->omit_vendor_cert = !check_vendor_cert;
 

src/cms_common.h
@@ -14,11 +14,15 @@
 #include <errno.h>
 #include <signal.h>
 #include <stdarg.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 #include <syslog.h>
 #include <time.h>
 #include <unistd.h>
 
 #include "util.h"
+#include "password.h"
 
 #define save_port_err() \
 	for (error_t saved_errno_0_ = 0, saved_errno_1_ = PORT_GetError(); saved_errno_0_ < 1; saved_errno_0_++, PORT_SetError(saved_errno_1_))
@@ -45,6 +49,44 @@ struct digest {
 	SECItem *pe_digest;
 };
 
+struct token_pass {
+	char *token;
+	char *pass;
+};
+
+struct pw_database {
+	struct token_pass *phrases;
+	size_t nphrases;
+};
+
+typedef enum {
+	// used only for bounds checking
+	PW_SOURCE_INVALID = 0,
+	// prompt the user (pwdata->data is NULL)
+	PW_PROMPT = 1,
+	// prompt the user to use a device (pwdata->data is NULL)
+	PW_DEVICE = 2,
+	// pwdata->data is plain text
+	PW_PLAINTEXT = 3,
+	// pwdata->data is a filename for a database
+	PW_FROMFILEDB = 4,
+	// pwdata->data is the database data
+	PW_DATABASE = 5,
+	// pwdata->data is the name of an environment variable
+	PW_FROMENV = 6,
+
+	// used only for bounds checking
+	PW_SOURCE_MAX
+} pw_source_t;
+
+typedef struct {
+	pw_source_t source;
+	pw_source_t orig_source;
+
+	struct pw_database pwdb;
+	char *data;
+} secuPWData;
+
 struct cms_context;
 
 typedef int (*cms_common_logger)(struct cms_context *, int priority,
@@ -57,8 +99,9 @@ typedef struct cms_context {
 	char *tokenname;
 	char *certname;
 	CERTCertificate *cert;
+	PK11SlotListElement *psle;
 	PK11PasswordFunc func;
-	void *pwdata;
+	secuPWData pwdata;
 
 	struct digest *digests;
 	int selected_digest;
@@ -145,23 +188,13 @@ extern SECOidTag digest_get_encryption_oid(cms_context *cms);
 extern SECOidTag digest_get_signature_oid(cms_context *cms);
 extern int digest_get_digest_size(cms_context *cms);
 extern void cms_set_pw_callback(cms_context *cms, PK11PasswordFunc func);
-extern void cms_set_pw_data(cms_context *cms, void *pwdata);
+extern void cms_set_pw_data(cms_context *cms, secuPWData *pwdata);
 
 extern int set_digest_parameters(cms_context *ctx, char *name);
 
 extern int generate_digest_begin(cms_context *cms);
 extern void generate_digest_step(cms_context *cms, void *data, size_t len);
 extern int generate_digest_finish(cms_context *cms);
-
-typedef struct {
-	enum {
-		PW_NONE = 0,
-		PW_FROMFILE = 1,
-		PW_PLAINTEXT = 2,
-		PW_EXTERNAL = 3
-	} source;
-	char *data;
-} secuPWData;
 
 #endif /* CMS_COMMON_H */
 // vim:fenc=utf-8:tw=75:noet

src/cms_pe_common.c
@@ -262,7 +262,7 @@ generate_digest(cms_context *cms, Pe *pe, int padded)
 			char *name = shdrs[i].name;
 			if (name && name[0] == '/')
 				name = get_str(pe, name + 1);
-			dprintf("section:\"%s\"\n", name);
+			dprintf("section:\"%s\"\n", name ? name : "(null)");
 			if (name && !strcmp(name, ".vendor_cert")) {
 				dprintf("skipping .vendor_cert section\n");
 				hashed_bytes += hash_size;

src/daemon.c
@@ -224,8 +224,14 @@ malformed:
 	if (!pin)
 		goto oom;
 
+	secuPWData pwdata;
+
+	memset(&pwdata, 0, sizeof(pwdata));
+	pwdata.source = pwdata.orig_source = PW_PLAINTEXT;
+	pwdata.data = pin;
+
 	cms_set_pw_callback(ctx->cms, get_password_passthrough);
-	cms_set_pw_data(ctx->cms, pin);
+	cms_set_pw_data(ctx->cms, &pwdata);
 
 	rc = unlock_nss_token(ctx->cms);
 

commit 17a80f7ee7a1d2d155b52d763ddf9f00c7d17d4a

Initialize pwdata in efikeygen and pesign
Fixes: github issue #105
Fixes: 12f1671 (Rework the wildly undocumented NSS password file goo.)
Complements: 1a4481e (Add more ways to use a password with the token)

Signed-off-by: Egor Ignatov <egori@altlinux.org>

src/cms_common.c
@@ -172,8 +172,10 @@ cms_context_fini(cms_context *cms)
 		xfree(cms->pwdata.data);
 		break;
 	case PW_PLAINTEXT:
-		memset(cms->pwdata.data, 0, strlen(cms->pwdata.data));
-		xfree(cms->pwdata.data);
+		if (cms->pwdata.data) {
+			memset(cms->pwdata.data, 0, strlen(cms->pwdata.data));
+			xfree(cms->pwdata.data);
+		}
 		break;
 	}
 	cms->pwdata.source = PW_SOURCE_INVALID;
@@ -319,8 +321,10 @@ void cms_set_pw_data(cms_context *cms, secuPWData *pwdata)
 	case PW_FROMENV:
 	case PW_FROMFILE:
 	case PW_PLAINTEXT:
-		memset(cms->pwdata.data, 0, strlen(cms->pwdata.data));
-		xfree(cms->pwdata.data);
+		if (cms->pwdata.data) {
+			memset(cms->pwdata.data, 0, strlen(cms->pwdata.data));
+			xfree(cms->pwdata.data);
+		}
 		break;
 
 	case PW_DATABASE:

src/efikeygen.c
@@ -985,6 +985,11 @@ int main(int argc, char *argv[])
 	if (!strcmp(dbdir, "-") && list_empty(&cms->pk12_ins) && !is_self_signed)
 		errx(1, "'--dbdir -' requires either --pk12-in or --self-sign.");
 
+	secuPWData pwdata;
+	memset(&pwdata, 0, sizeof(pwdata));
+	pwdata.source = pwdata.orig_source = PW_PROMPT;
+	cms_set_pw_data(cms, &pwdata);
+
 	PK11_SetPasswordFunc(cms->func ? cms->func : readpw);
 	if (strcmp(dbdir, "-")) {
 		if (cms->pk12_out.fd >= 0)

src/pesign.c
@@ -395,6 +395,8 @@ main(int argc, char *argv[])
 		pwdata.data = strdup(secure_getenv("PESIGN_TOKEN_PIN"));
 		if (!pwdata.data)
 			err(1, "could not allocate memory");
+	} else if (pwdata.source == PW_SOURCE_INVALID) {
+		pwdata.source = PW_PROMPT;
 	}
 	pwdata.orig_source = pwdata.source;
 